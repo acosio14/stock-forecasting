25751769184c49b796a8190c890c8f29caa1e054
Adrian Cosio <adriancosio14@gmail.com>
2025-12-01 06:49:02 -0600

    Add normalize function

--- data_handler.py
+++ data_handler.py
@@ -6,6 +6,11 @@ nvidia_data = yf.Ticker("NVDA")
 nvda_price_df = nvidia_data.history(period='max')
 nvda_price_df.to_csv('data/nvidia_stock_price_max_hist.csv')
 
+
+def normalize(array):
+    return (array - np.min(array)) / (np.max(array) - np.min(array))
+
+
 def sliding_window(array):
     # Imporvement: Add variable window_length size
     """Sliding window to of size 3."""
