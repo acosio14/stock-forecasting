b2ae12cfac0a7639be24960c66db829d7fc02e78
Adrian Cosio <adriancosio14@gmail.com>
2025-12-01 06:44:58 -0600

    Add sliding window function to create timesteps for sequence models.

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,13 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
 nvidia_data = yf.Ticker("NVDA")
 nvda_price_df = nvidia_data.history(period='max')
 nvda_price_df.to_csv('data/nvidia_stock_price_max_hist.csv')
+
+def sliding_window(array):
+    # Imporvement: Add variable window_length size
+    """Sliding window to of size 3."""
+    return np.column_stack([array[0:-2]], array[1:-1], array[2:])
+
