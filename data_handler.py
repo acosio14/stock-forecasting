0b43559b645970e070b8414343dd2cd8bb881d09
Adrian Cosio <adriancosio14@gmail.com>
2025-12-02 05:32:43 -0600

    Convert to function with proper type hints.

--- data_handler.py
+++ data_handler.py
@@ -1,18 +1,25 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from numpy.typing import NDArray
+from pathlib import Path
 
-nvidia_data = yf.Ticker("NVDA")
-nvda_price_df = nvidia_data.history(period='max')
-nvda_price_df.to_csv('data/nvidia_stock_price_max_hist.csv')
+def extract_stock_close_price(
+        ticker_name: str, period_length: str, filepath: str, columns: list[str],
+) -> NDArray[np.float64]:
+    """Extract the specific features from the desired stock through yahoo finance."""
+    stock_data = yf.Ticker(ticker_name)
+    close_price_df = stock_data.history(period=period_length)
+    if not Path(filepath).exists():
+        close_price_df.to_csv(filepath)
+    return close_price_df[columns].to_numpy()
 
-
-def normalize(array):
+def normalize(array: NDArray[np.float64]):
+    """Normalize the data."""
     return (array - np.min(array)) / (np.max(array) - np.min(array))
 
-
-def sliding_window(array):
+def sliding_window(array: NDArray[np.float64]):
     # Imporvement: Add variable window_length size
-    """Sliding window to of size 3."""
+    """Sliding window of size 3."""
     return np.column_stack([array[0:-2]], array[1:-1], array[2:])
 
