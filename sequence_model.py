ffd09f2fc6e2ab32adfe05f5937036fe306d61fb
Adrian Cosio <adriancosio14@gmail.com>
2025-12-11 06:13:10 -0600

    Add initial setup for training loop function.

--- sequence_model.py
+++ sequence_model.py
@@ -1,6 +1,7 @@
 # Sequence models
 import torch
 import torch.nn as nn
+import torch.optim
 
 
 class SimpleRNN(nn.Module):
@@ -29,14 +30,27 @@ class SimpleLSTM(nn.Module):
         return y_hat
 
 
-class SimpleGRU(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.rnn = nn.GRU(input_size=1, hidden_size=8, num_layers=1, batch_first=True)
-        self.fc = nn.Linear(in_features=8, out_features=1)
+class myLSTM():
+    """LSTM from scratch."""
 
-    def forward(self, input):  # x (batch, seq_len, input_size)
-        x = self.rnn(input)
-        y_hat = self.fc(x)
 
-        return y_hat
+def train_model(model, num_epochs, num_batches, X, y):
+    """Train Neural Network."""
+
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    for iterable in iteration:
+        optimizer.zero_grad()
+
+        y_hat = model(X,y)
+        loss = loss_function(y_hat, y)
+
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss
+
+
+
+
