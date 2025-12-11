c3d32ba4a80427b51d7c7999360fe3b04688e254
Adrian Cosio <adriancosio14@gmail.com>
2025-12-11 05:31:52 -0600

    ruff format

--- sequence_model.py
+++ sequence_model.py
@@ -9,12 +9,12 @@ class RecurrentNeuralNet(nn.Module):
         self.rnn = nn.RNN(input_size=1, hidden_size=8, num_layers=1, batch_first=True)
         self.fc = nn.Linear(in_features=8, out_features=1)
 
-    
-    def forward(self,input): # x (batch, seq_len, input_size)
+    def forward(self, input):  # x (batch, seq_len, input_size)
         x = self.rnn(input)
         y_hat = self.fc(x)
 
         return y_hat
+
 
 class LSTMNet(nn.Module):
     def __init__(self) -> None:
@@ -22,12 +22,12 @@ class LSTMNet(nn.Module):
         self.rnn = nn.LSTM(input_size=1, hidden_size=8, num_layers=1, batch_first=True)
         self.fc = nn.Linear(in_features=8, out_features=1)
 
-    
-    def forward(self,input): # x (batch, seq_len, input_size)
+    def forward(self, input):  # x (batch, seq_len, input_size)
         x = self.rnn(input)
         y_hat = self.fc(x)
 
         return y_hat
+
 
 class GRUNet(nn.Module):
     def __init__(self) -> None:
@@ -35,8 +35,7 @@ class GRUNet(nn.Module):
         self.rnn = nn.GRU(input_size=1, hidden_size=8, num_layers=1, batch_first=True)
         self.fc = nn.Linear(in_features=8, out_features=1)
 
-    
-    def forward(self,input): # x (batch, seq_len, input_size)
+    def forward(self, input):  # x (batch, seq_len, input_size)
         x = self.rnn(input)
         y_hat = self.fc(x)
 
