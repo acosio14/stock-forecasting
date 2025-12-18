c6f213f9d1d638b33910d8d44ebb9e8f84565cbd
Adrian Cosio <adriancosio14@gmail.com>
2025-12-18 06:31:43 -0600

    Update output of SimpleLSTM class.

--- sequence_model.py
+++ sequence_model.py
@@ -23,12 +23,12 @@ class SimpleRNN(nn.Module):
 class SimpleLSTM(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.rnn = nn.LSTM(input_size=1, hidden_size=8, num_layers=1, batch_first=True)
+        self.lstm = nn.LSTM(input_size=1, hidden_size=8, num_layers=1, batch_first=True)
         self.fc = nn.Linear(in_features=8, out_features=1)
 
     def forward(self, input):  # x (batch, seq_len, input_size)
-        x = self.rnn(input)
-        y_hat = self.fc(x)
+        output, (hx, cx) = self.lstm(input)
+        y_hat = self.fc(output[:,-1,:])
 
         return y_hat
 
